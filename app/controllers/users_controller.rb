a70bca225f571d5efe5728ad4a50186b57db8ca3
stolarz92 <stolarz92@gmail.com>
2020-12-19 22:58:59 +0100

    Add strong_params to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,13 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create, :destroy]
 
   def create
+    Events::User::Created.create(payload: user_params)
   end
 
   def destroy
+  end
+
+  private def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
