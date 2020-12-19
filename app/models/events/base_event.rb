957e3231786d51bfd109a5ded9ee2829474e99c5
stolarz92 <stolarz92@gmail.com>
2020-12-19 21:17:32 +0100

    Add apply_aggregate and apply_and_persist methods

--- app/models/events/base_event.rb
+++ app/models/events/base_event.rb
@@ -1,3 +1,23 @@
 class Events::BaseEvent < ActiveRecord::Base
   self.abstract_class = true
+
+  before_create :apply_and_persist
+
+  def apply(aggregate)
+    raise NotImplementedError
+  end
+
+  private def apply_and_persist
+    # Lock the database row! (OK because we're in an ActiveRecord callback chain transaction)
+    aggregate.lock! if aggregate.persisted?
+
+    # Apply!
+    self.aggregate = apply(aggregate)
+
+    #Persist!
+    aggregate.save!
+
+    # Update aggregate_id with id from newly created User
+    self.aggregate_id = aggregate.id if aggregate_id.nil?
+  end
 end
