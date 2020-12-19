35910a9660a2926705532a672e0c02c9ed33e3e1
stolarz92 <stolarz92@gmail.com>
2020-12-19 22:46:46 +0100

    Add user_events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_180951) do
+ActiveRecord::Schema.define(version: 2020_12_19_214528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_events", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "event_type"
+    t.json "payload"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_12_19_180951) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_events", "users"
 end
